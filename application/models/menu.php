8d54d41a65f70383e17c8d173715b4041ad280b9
Skiv <just.skiv@gmail.com>
2011-06-24 02:09:15 +0600

    menu model

--- application/models/menu.php
+++ application/models/menu.php
@@ -4,5 +4,23 @@
  * Date: 24.06.11
  * Time: 1:21
  */
-    
-?>
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Menu extends CI_Model
+{
+  function Menu()
+  {
+    parent::__construct();
+    //__construct
+  }
+
+  function getMenuNames()
+  {
+    $query = $this->db->query('SELECT * FROM menu');
+    return $query->result_array();
+  }
+}
+
+/* End of file crud.php */
+/* Location: ./application/controllers/crud.php */

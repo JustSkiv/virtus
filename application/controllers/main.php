94bad4c9ad77136e8d24d99ee8d6310f4f99933b
Skiv <just.skiv@gmail.com>
2011-06-24 02:08:26 +0600

    main page

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -8,13 +8,12 @@
 class Main extends CI_Controller {
   public function index()
 	{
-    $userdata = 'aaaaaa';
 		$this->load->library('templates');
-    
+    $this->load->model('menu');
+
     $resource_dir = $this->config->item('resource_dir');
     $this->templates->assign('resource_dir', $resource_dir);
-
-    $this->templates->assign('username', $userdata); //объявляем переменную
+    $this->templates->assign('menu',  $this->menu->getMenuNames());
     $this->templates->display('index.tpl'); //указываем имя шаблона
 	}
 }

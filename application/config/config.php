10bf1de9eaa8873d0cbca2ad1523adc9c788bfeb
Skiv <just.skiv@gmail.com>
2011-07-06 01:03:05 +0600

    fixes

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ $config['resource_dir'] = $config['base_url'] . 'resources';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
